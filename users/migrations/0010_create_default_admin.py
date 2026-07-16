cab333cc99151a53223687e72481d09d0c4c4ea7
GlennPena <gppena.student@ua.edu.ph>
2026-07-17 01:41:39 +0800

    Use make_password helper in migrations

--- users/migrations/0010_create_default_admin.py
+++ users/migrations/0010_create_default_admin.py
@@ -1,19 +1,20 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 def create_default_admin(apps, schema_editor):
     User = apps.get_model('users', 'User')
     if not User.objects.filter(username='admin').exists():
         admin_user = User(
             username='admin',
-            email='admin@ua.edu.ph',
+            email='uacitappointment@gmail.com',
             first_name='System',
             last_name='Admin',
             role='admin',
             is_superuser=True,
             is_staff=True,
-            is_active=True
+            is_active=True,
+            password=make_password('admin123')
         )
-        admin_user.set_password('admin123')
         admin_user.save()
 
 class Migration(migrations.Migration):
